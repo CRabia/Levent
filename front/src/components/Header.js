f442fc567e20efb5489761e5d0b0c80ac90f00af
CRabia <ketby000@gmail.com>
2020-02-06 10:17:04 +0100

    feat(header): make animation

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -2,31 +2,43 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 export default class Header extends Component {
-  async componentDidMount() {
-    window.addEventListener("scroll", this.handleScroll);
-  }
+    state = {
+        classMenu: "transparent-nav-bar"
+    };
 
-  render() {
-    return (
-      <div className="menu">
-        <div className="item-nav vertical-center">
-          <Link to="/contact">
-            <p>Contact</p>
-          </Link>
-        </div>
+    componentDidMount = async () => {
+        window.addEventListener("scroll", this.handleScroll);
+    };
 
-        <div className="item-nav vertical-center">
-          <Link to="/connexion">
-            <p>Connexion</p>
-          </Link>
-        </div>
+    handleScroll = e => {
+        const classMenu =
+            window.scrollY > 3 ? "opaque-nav-bar" : "transparent-nav-bar";
+        this.setState({ classMenu });
+    };
 
-        <div className="item-nav vertical-center">
-          <Link to="/inscription">
-            <button className=" btn-rounded-100 btn-violet">Inscription</button>
-          </Link>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div id="menu" className={`${this.state.classMenu}`}>
+                <div className="item-nav vertical-center">
+                    <Link to="/contact">
+                        <p>Contact</p>
+                    </Link>
+                </div>
+
+                <div className="item-nav vertical-center">
+                    <Link to="/connexion">
+                        <p>Connexion</p>
+                    </Link>
+                </div>
+
+                <div className="item-nav vertical-center">
+                    <Link to="/inscription">
+                        <button className=" btn-rounded-100 btn-violet">
+                            Inscription
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        );
+    }
 }
--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -4,42 +4,42 @@ import { Link } from "react-router-dom";
 import logo from "../logo.png";
 
 export default class Home extends Component {
-  async componentDidMount() {}
+    render() {
+        return (
+            <div id="homepage">
+                <section className="banner-home">
+                    <div className="content">
+                        <img src={logo} className="logo" />
+                        <h1>
+                            Découvrez toutes les activitées près de chez vous
+                        </h1>
+                        <p>
+                            En vous inscrivant vous pourrez participer à pleins
+                            d'activitées près de chez vous !
+                        </p>
+                        <div className="container-button">
+                            <Link to="/inscription">
+                                <button className="btn-rounded-100 btn-violet">
+                                    Inscrivez-vous
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
 
-  render() {
-    return (
-      <div id="homepage">
-        <section className="banner-home">
-          <div className="content">
-            <img src={logo} className="logo" />
-            <h1>Découvrez toutes les activitées près de chez vous</h1>
-            <p>
-              En vous inscrivant vous pourrez participer à pleins d'activitées
-              près de chez vous !
-            </p>
-            <div className="container-button">
-              <Link to="/inscription">
-                <button className="btn-rounded-100 btn-violet">
-                  Inscrivez-vous
-                </button>
-              </Link>
+                    <div className="illustration">
+                        <div className="display-card position-top">
+                            <Card />
+                        </div>
+                        <div className="display-card">
+                            <Card />
+                            <Card />
+                        </div>
+                        <div className="display-card position-bottom">
+                            <Card />
+                        </div>
+                    </div>
+                </section>
             </div>
-          </div>
-
-          <div className="illustration">
-            <div className="display-card position-top">
-              <Card />
-            </div>
-            <div className="display-card">
-              <Card />
-              <Card />
-            </div>
-            <div className="display-card position-bottom">
-              <Card />
-            </div>
-          </div>
-        </section>
-      </div>
-    );
-  }
+        );
+    }
 }
