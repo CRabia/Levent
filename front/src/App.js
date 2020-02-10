a09f6e2da1c425d2cb1df01807f2e7684b1a929f
CRabia <ketby000@gmail.com>
2020-02-10 23:56:28 +0100

    wip(front): render header and footer && context

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import React, { Component, useEffect, useState } from "react";
+import { Router, Route, useLocation, Switch } from "react-router-dom";
 import "./App.scss";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
@@ -7,34 +7,79 @@ import Login from "./pages/Login";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { AuthProvider } from "./contexts/auth.context";
+import { createBrowserHistory } from "history";
 
-export default class App extends Component {
-    state = {
-        renderHender: true,
-        renderFooter: true
-    };
+const routes = ["/connexion", "/inscription"];
+const customHistory = createBrowserHistory();
 
-    componentDidMount() {
-        let routes = ["/connexion", "/inscription"];
-        !routes.includes(window.location.pathname)
-            ? this.setState({ renderFooter: true, renderHender: true })
-            : this.setState({ renderFooter: false, renderHender: false });
+const App = () => {
+  const [renderHeader, setRenderHeader] = useState(true);
+  const [renderFooter, setRenderFooter] = useState(true);
+
+  useEffect(() => {
+    displayNavigationLayout();
+    customHistory.listen(() => displayNavigationLayout());
+  }, []);
+
+  const displayNavigationLayout = () => {
+    if (!routes.includes(customHistory.location.pathname)) {
+      setRenderFooter(true);
+      setRenderHeader(true);
+    } else {
+      setRenderFooter(false);
+      setRenderHeader(false);
     }
+  };
 
-    render() {
-        return (
-            <AuthProvider>
-                <BrowserRouter>
-                    {this.state.renderHender && <Header />}
+  return (
+    <AuthProvider>
+      <Router history={customHistory}>
+        {renderHeader && <Header />}
+        <Route path="/" exact component={Home} />
+        <Route path="/contact" exact component={Contact} />
+        <Route path="/inscription" exact component={Login} />
+        <Route path="/connexion" exact component={Login} />
+        {renderFooter && <Footer />}
+      </Router>
+    </AuthProvider>
+  );
+};
 
-                    <Route path="/" exact component={Home} />
-                    <Route path="/contact" exact component={Contact} />
-                    <Route path="/inscription" exact component={Login} />
-                    <Route path="/connexion" exact component={Login} />
+export default App;
 
-                    {this.state.renderFooter && <Footer />}
-                </BrowserRouter>
-            </AuthProvider>
-        );
-    }
-}
+// export default class App extends Component {
+//   state = {
+//     renderHeader: true,
+//     renderFooter: true
+//   };
+
+//   componentDidMount() {
+//     this.displayNavigationLayout();
+//     customHistory.listen(() => {
+//       this.displayNavigationLayout();
+//     });
+//   }
+
+//   displayNavigationLayout() {
+//     !routes.includes(window.location.pathname)
+//       ? this.setState({ renderFooter: true, renderHeader: true })
+//       : this.setState({ renderFooter: false, renderHeader: false });
+//   }
+
+//   render() {
+//     return (
+//       <AuthProvider>
+//         <Router history={customHistory}>
+//           {this.state.renderHeader && <Header />}
+
+//           <Route path="/" exact component={Home} />
+//           <Route path="/contact" exact component={Contact} />
+//           <Route path="/inscription" exact component={Login} />
+//           <Route path="/connexion" exact component={Login} />
+
+//           {this.state.renderFooter && <Footer />}
+//         </Router>
+//       </AuthProvider>
+//     );
+//   }
+// }
--- front/src/contexts/auth.context.js
+++ front/src/contexts/auth.context.js
@@ -3,75 +3,76 @@ import AuthService from "../services/auth.service";
 
 const AuthContext = React.createContext();
 
-const resetErrorMessage = {
-    emailErrorMessage: "",
-    firstnameErrorMessage: "",
-    lastnameErrorMessage: "",
-    passwordErrorMessage: "",
-    errorMessage: ""
+const modelErrorMessage = {
+  emailErrorMessage: "",
+  firstnameErrorMessage: "",
+  lastnameErrorMessage: "",
+  passwordErrorMessage: "",
+  errorMessage: ""
 };
 
 export class AuthProvider extends Component {
-    state = {
-        errorMessage: "",
-        emailErrorMessage: "",
-        firstnameErrorMessage: "",
-        lastnameErrorMessage: "",
-        passwordErrorMessage: ""
-    };
+  state = {
+    errorMessage: "",
+    emailErrorMessage: "",
+    firstnameErrorMessage: "",
+    lastnameErrorMessage: "",
+    passwordErrorMessage: ""
+  };
 
-    errorsDisplay = errors => {
-        this.setState(resetErrorMessage);
-        errors.forEach(element => {
-            this.setState({ [element.param + "ErrorMessage"]: element.msg });
-        });
-    };
+  resetErrorMessage = () => {
+    this.setState(modelErrorMessage);
+  };
 
-    userAuthentification = async body => {
-        let response = await AuthService.auth(body);
-        let data = await response.json();
-        if (response.ok) {
-            localStorage.setItem("token", data.token);
-        } else {
-            this.setState({ errorMessage: data.message });
-        }
-    };
+  errorsDisplay = errors => {
+    this.resetErrorMessage();
+    errors.forEach(element => {
+      this.setState({ [element.param + "ErrorMessage"]: element.msg });
+    });
+  };
 
-    userRegistration = async body => {
-        let response = await AuthService.register(body);
-        let data = await response.json();
-        if (response.ok) {
-            this.userAuthentification(body);
-        } else {
-            this.errorsDisplay(data.errors);
-        }
-    };
-
-    submit = async (e, body, request) => {
-        e.preventDefault();
-        if (request === "authentificate") {
-            this.userAuthentification(body);
-        } else {
-            this.userRegistration(body);
-        }
-    };
-
-    render() {
-        return (
-            <AuthContext.Provider
-                value={{
-                    submit: this.submit,
-                    firstnameErrorMessage: this.state.firstnameErrorMessage,
-                    lastnameErrorMessage: this.state.lastnameErrorMessage,
-                    passwordErrorMessage: this.state.passwordErrorMessage,
-                    emailErrorMessage: this.state.emailErrorMessage,
-                    errorMessage: this.state.errorMessage
-                }}
-            >
-                {this.props.children}
-            </AuthContext.Provider>
-        );
+  userAuthentication = async body => {
+    let response = await AuthService.auth(body);
+    let data = await response.json();
+    if (response.ok) {
+      localStorage.setItem("token", data.token);
+    } else {
+      this.setState({ errorMessage: data.message });
     }
+  };
+
+  userRegistration = async body => {
+    let response = await AuthService.register(body);
+    let data = await response.json();
+    if (response.ok) {
+      this.userAuthentication(body);
+    } else {
+      this.errorsDisplay(data.errors);
+    }
+  };
+
+  submit = async (e, body, request) => {
+    e.preventDefault();
+    if (request === "authentificate") {
+      this.userAuthentication(body);
+    } else {
+      this.userRegistration(body);
+    }
+  };
+
+  render() {
+    return (
+      <AuthContext.Provider
+        value={{
+          submit: this.submit,
+          resetErrorMessage: this.resetErrorMessage,
+          ...this.state
+        }}
+      >
+        {this.props.children}
+      </AuthContext.Provider>
+    );
+  }
 }
 
 export default AuthContext;
