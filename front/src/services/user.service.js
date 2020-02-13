784d7ea49d3e24cbb65dd5fc993b285b109ac24d
CRabia <ketby000@gmail.com>
2020-02-13 21:24:02 +0100

    wip(admin): design dashboard page

---
+++ front/src/services/user.service.js
@@ -0,0 +1,52 @@
+const baseUrl = "http://localhost:3001";
+
+export default class UserService {
+    static async list() {
+        let init = {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+        };
+        let call = await fetch(`${baseUrl}/users`, init);
+        return call;
+    }
+
+    static async details(id) {
+        let init = {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+        };
+        let call = await fetch(`${baseUrl}/users/${id}`, init);
+        return call;
+    }
+
+    static async update(id, body) {
+        let init = {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            },
+            body: JSON.stringify(body)
+        };
+        let call = await fetch(`${baseUrl}/users/${id}`, init);
+        return call;
+    }
+
+    static async delete(id) {
+        let init = {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+        };
+        let call = await fetch(`${baseUrl}/users/${id}`, init);
+        return call;
+    }
+}
