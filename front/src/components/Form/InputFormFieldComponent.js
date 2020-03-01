3cd9842f4270b275bf1df15c0667dc26282fad3d
Cyril Rabia <cyril@prepacademy.fr>
2020-03-02 00:12:52 +0100

    refacto(inputComponent): add type

--- front/src/components/Form/InputFormFieldComponent.js
+++ front/src/components/Form/InputFormFieldComponent.js
@@ -2,16 +2,17 @@ import React from "react";
 
 const InputFormField = props => {
     const callBack = props.callBack;
-    const value = props.value;
     const textField = props.textField;
+    const type = props.type;
+    const value = props.value;
 
     return (
         <div className="display-form-admin">
             <div className="label">{textField}</div>
             {callBack !== "" ? (
-                <input type="text" value={value} onChange={e => callBack(e.target.value)} autoComplete="off" />
+                <input type={type} value={value} onChange={e => callBack(e.target.value)} autoComplete="off" />
             ) : (
-                <input type="text" value={value} readOnly autoComplete="off" />
+                <input type={type} value={value} readOnly autoComplete="off" />
             )}
         </div>
     );
